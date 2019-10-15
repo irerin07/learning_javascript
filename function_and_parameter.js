aa8941171bad3de230ebbc9041d5532836d1a253
irerin07 <counterchord89@gmail.com>
2019-10-15 19:03:55 +0900

    function

--- function_and_parameter.js
+++ function_and_parameter.js
@@ -44,7 +44,7 @@ function h(p) {
 let p = {
   message: "initial value"
 };
-
+console.log();
 console.log(`h를 호출하기 전: p.message = "${p.message}"`); //1. h를 호출하기 전: p.message = "initial value"
 h(p);
 console.log(`h를 호출한 다음: p.message="${p.message}"`); //3. h를 호출한 다음: p.message="h에서 수정함"
@@ -52,5 +52,166 @@ console.log(`h를 호출한 다음: p.message="${p.message}"`); //3. h를 호출
 /*
 해당 코드의 핵심은 함수 내부의 매개변수 p와 함수 바깥의 변수 p가 다르다는 것이다.
 h를 호출하면 둘은 같은 객체를 가리키지만, h 내부에서 p에 할당한 객체는 새로운 객체이다.
-함수 바깥의 p는 여전히 원래 객케를 가리키고 있다.
+함수 바깥의 p는 여전히 원래 객체를 가리키고 있다.
 */
+function i(q) {
+  this.q = q;
+  console.log(this.q);
+  q.message = "h에서 수정함";
+  console.log(this.q);
+  q = {
+    message: "새로운 객체 할당"
+  };
+  console.log(this.q);
+  q.message = "new Message";
+  console.log(this.q);
+  q = {
+    message: "another new message"
+  };
+  console.log(this.q);
+  q.message = "and another new message";
+  console.log(this.q);
+  console.log(`i 내부: q.message="${q.message}" (할당후)`);
+}
+
+let q = {
+  message: "initial value"
+};
+console.log();
+console.log(`i를 호출하기 전: q.message = "${q.message}"`);
+i(q);
+console.log(`i를 호출한 다음: q.message="${q.message}"`);
+
+function j(x) {
+  return `in j: x = ${x}`;
+}
+
+console.log(j());
+/*
+자바스크립트에서는 매개변수가 함수를 결정하지 않는다.
+정해진 매개변수에 값을 제공하지 않으면 암시적으로 undefuned가 할당된다.
+*/
+
+function getSentence({ subject, verb, object }) {
+  return `${subject} ${verb} ${object}`;
+}
+
+const o1 = {
+  subject: "I",
+  verb: "love",
+  object: "Javascript"
+};
+
+console.log(getSentence(o1));
+//객체를 변수로 해체
+
+function getSentence1([subject, verb, object]) {
+  return `${subject} ${verb} ${object}`;
+}
+
+const arr = ["I", "Love", "Javascript"];
+console.log(getSentence1(arr));
+//배열을 변수로 해체
+
+function addPrefix(prefix, ...words) {
+  const prefixedWords = [];
+  for (let i = 0; i < words.length; i++) {
+    prefixedWords[i] = prefix + words[i];
+  }
+  return prefixedWords;
+}
+console.log(addPrefix("con", "verse", "vex"));
+/*확산 연산자(...)를 써서 남는 매개변수를 이용한다
+함수를 선언할 때 확산 연산자는 반드시 마지막 매개변수여야 한다. 
+그렇지 않으면 전달된 값 중 어디까지를 확산 매개변수에 할당해야 하는지 판단할 수 없어서 에러가 난다.
+*/
+
+function f(a, b = "default", c = 3) {
+  return `${a} - ${b} - ${c}`;
+}
+
+console.log(f(5, 6, 7));
+console.log(f(5, 6));
+console.log(f(5));
+console.log(f());
+/*
+ES6에서는 매개변수에 default value를 지정하는 기능도 추가가 되었다.
+일반적으로 매개변수에 값을 제공하지 않으면 undefined가 되지만 기본값을 지정할 수 있게 되었다.
+*/
+
+const o2 = {
+  name: "Wallace", //원시값 프로퍼티
+  bark: function() {
+    //함수 프로퍼티(메서드)
+    return "Woof";
+  }
+};
+
+const o3 = {
+  name: "Wallace",
+  bark() {
+    //ES6에서 추가된 간편하게 메서드를 추가할 수 있는 문법.
+    return "Woof";
+  }
+};
+
+const o4 = {
+  name: "Wallace",
+  speak() {
+    return `My name is ${this.name}`;
+  }
+};
+console.log(o4.speak());
+/*
+this의 값은 어떻게 선언했느냐가 아니라 어떻게 호출했느냐에 따라 달라진다는 것을 이해해야한다.
+위의 예제에서 this가 o4에 묶인 이유는 o4에서 speak를 호출했기 때문이다.
+*/
+const speak = o4.speak;
+console.log(speak === o4.speak); //true
+console.log(speak()); // My name is undefined
+/*
+함수를 speak(); 와 같이 호출을 하게 되면 이 함수가 어디에 속하는지 알 수 없으므로 this는 undefuned에 묶인다.
+*/
+
+/*
+const o5 = {
+  name: "Julie",
+  greetBackWards: function() {
+    console.log(this.name);
+    function getReverseName() {
+      console.log(this.name);
+      let nameBackWards = "";
+      for (let i = this.name.length - 1; i >= 0; i--) {
+        nameBackWards += this.name[i];
+      }
+      return nameBackWards;
+    }
+    return `${getReverseName()} si eman ym ,olleH`;
+  }
+};
+
+console.log(o5.greetBackWards());
+*/
+/*
+위 코드는 의도한대로 작동하지 않는다.
+o.greetBackWards()를 호출하는 시점에서 자바스크립트는 this를 의도대로 o5에 연결하지만 ,greetBackWards안에서 getReverseName을 호출하면 this는 o5가 아닌 다른것에 묶인다.
+이를 해결하기 위해 this를 다른 변수에 할당하는 것.
+*/
+
+const o6 = {
+  name: "Julie",
+  greetBackWards: function() {
+    console.log(this.name);
+    const self = this;
+    function getReverseName() {
+      console.log(self.name);
+      let nameBackWards = "";
+      for (let i = self.name.length - 1; i >= 0; i--) {
+        nameBackWards += self.name[i];
+      }
+      return nameBackWards;
+    }
+    return `${getReverseName()} si eman ym ,olleH`;
+  }
+};
+console.log(o6.greetBackWards());
