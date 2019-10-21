cb17f9180f2dc21a3cc70ab9d6bd7c1a0c6b7a75
irerin07 <counterchord89@gmail.com>
2019-10-21 18:12:10 +0900

    arrays

--- arrays.js
+++ arrays.js
@@ -102,3 +102,82 @@ function cardToString(c) {
 
 cards.filter(c => c.value === 2).map(cardToString);
 cards.filter(c => c.value > 10 && c.suit === "H").map(cardToString);
+
+/*
+reduce는 배열 자체를 변형한다.
+콜백 함수를 받는다.
+reduce가 받는 첮번째 매개변수는 배열이 줄어드는 대상인 어큐뮬레이터accumularot이다.
+두번쨰 부터는 콜백의 순서대로 현재 배열요소, 현재 인덱스, 배열 자제이다.
+reduce는 초깃값도 옵션으로 받을 수 있다.
+*/
+const arr2 = [5, 7, 2, 4];
+const sum1 = arr2.reduce((a, x) => (a += x), 0);
+console.log(sum1);
+/*
+reduce의 콜백 함수는 매개변수로 누적값 a와 현재 배열 요소 x를 받았다.
+누적값은 0에서 시작한다.
+
+1.첫 번째 배열 요소 5에서 (익명)함수를 호출. a의 초깃값은 0이고 x의 값은 5이다. 함수는 a와 x의 합을 반환한다. 이 값은 다음 단계에서 a의 값이 된다.
+2.두 번째 배열 요소 7에서 함수를 호출, a의 초깃값은 이제 이전 단계에서 전달한 5가 되었고 x의 값은 7이다. 함수는 a와 x를 합해 합 12를 반환한다.
+3.세 번째 배열 요소 2에서 함수를 호출, a의 초깃값은 이전 단계에서 전달한 12이고 x는 2이다. 함수는 a와 x를 합한 합 14를 반환한다.
+4.네 번째 배열 요소 4에서 함수를 로출, a의 초깃값은 이전 단계에서 전달한 14이고 x는 4이다. 함수는 이 둘을 합한 합 18을 반환한다.
+
+초기값이 undefined인 상태에서 시작한다면
+const arr2 = [5, 7, 2, 4];
+const sum1 = arr2.reduce((a, x) => a += x);
+
+a의 초깃값은 첫번쨰 배열 요소인 5가 된다.
+
+reduce는 보통 숫자나 문자열 같은 원시 값을 누적값으로 사용하지만 객체 또한 누적값이 될 수 있다.
+*/
+const words = [
+  "BeachBall",
+  "Rodeo",
+  "Angel",
+  "Aardvark",
+  "Xylophone",
+  "November",
+  "Chocolate",
+  "Papaya",
+  "Uniform",
+  "Joker",
+  "Clover",
+  "Bali"
+];
+const alphabetical = words.reduce((a, x) => {
+  if (!a[x[0]]) a[x[0]] = [];
+  a[x[0]].push(x);
+  return a;
+}, {});
+
+console.log(alphabetical);
+/*
+영단어로 이루어진 배열에서 각 단어를 첫 글자에 따라 묶는 코드
+배열의 모든 요소에서 콜백 함수는 전 단계의 결과에 이 단어의 첫 번쨰 글자인 프로퍼티가 있는지 확인한다.
+만약 프로퍼티가 없다면 빈 배열을 추가한다.
+즉, "BeachBall"을 만나면 a.B 프로퍼티를 확인하는 데 그런 프로퍼티는 없으므로 빈 배열을 만든다.
+그리고 해당 단어를 적절한 배열에 추가한다.
+BeachBall은 a.B프로퍼티가 없었으므로 빈 배열에 추가되고 마지막으로 {B: [BeachBall]}인 a를 반환한다.
+*/
+
+const data = [3.3, 5, 7.2, 12, 4, 6, 10.3];
+const stats = data.reduce(
+  (a, x) => {
+    a.N++;
+    let delta = x - a.mean;
+    a.mean += delta / a.N;
+    a.M2 += delta * (x - a.mean);
+    return a;
+  },
+  { N: 0, mean: 0, M2: 0 }
+);
+if (stats.N > 2) {
+  stats.variance = stats.M2 / (stats.N - 1);
+  stats.stdev = Math.sqrt(stats.variance);
+}
+console.log(stats);
+
+const longwords = words
+  .reduce((a, w) => (w.length > 6 ? a + " " + w : a), "")
+  .trim();
+console.log(longwords);
